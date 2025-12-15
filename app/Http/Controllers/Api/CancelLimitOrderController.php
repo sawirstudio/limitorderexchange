1816b0c895e9a7b44c31283f21eb1169016c86f3
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2025-12-16 05:47:03 +0700

    fix(order): handle cancel and release assets or balance

--- app/Http/Controllers/Api/CancelLimitOrderController.php
+++ app/Http/Controllers/Api/CancelLimitOrderController.php
@@ -4,15 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Enums\OrderStatus;
 use App\Http\Controllers\Controller;
+use App\Models\Asset;
 use App\Models\Order;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\ValidationException;
 
 class CancelLimitOrderController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
-    public function __invoke(Order $order)
+    public function __invoke(Order $order, Request $request)
     {
         match ($order->status) {
             OrderStatus::CANCELED => throw ValidationException::withMessages([
@@ -21,10 +21,19 @@ class CancelLimitOrderController extends Controller
             OrderStatus::FILLED => throw ValidationException::withMessages([
                 'status' => ['Order is already filled'],
             ]),
-            OrderStatus::OPEN => $order->update([
-                'status' => OrderStatus::CANCELED,
-            ]),
+            default => null,
         };
+
+        DB::select("with ord as (
+        update orders set status = 3 where id = ? returning user_id, symbol, side, (price * amount) as volume
+        ),
+        release_locked_asset as (
+            update assets set amount = amount + locked_amount, locked_amount = null where exists(select 1 from ord where ord.side is false and ord.user_id = assets.user_id and ord.symbol = assets.symbol) returning amount
+        ),
+        release_locked_balance as (
+            update users set balance = balance + (select volume from ord) where exists(select 1 from ord where ord.side is true and ord.user_id = users.id) returning balance
+        )
+        select 1", [$order->getKey()]);
 
         return response()->noContent();
     }
--- tests/Feature/Http/Controllers/Api/CancelLimitOrderControllerTest.php
+++ tests/Feature/Http/Controllers/Api/CancelLimitOrderControllerTest.php
@@ -1,13 +1,34 @@
 <?php
 
+use App\Enums\OrderStatus;
+use App\Models\Asset;
 use App\Models\Order;
 
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\postJson;
 
-test('cancel order', function () {
+test('cancel sell order', function () {
     actingAs($user = user());
-    $order = Order::factory()->for($user)->create();
+    $order = Order::factory()->for($user)->create(['status' => OrderStatus::OPEN, 'side' => false]);
+    $asset = Asset::factory()->for($user)->create(['locked_amount' => $order->amount, 'symbol' => $order->symbol]);
+    $initialAmount = $asset->amount;
     postJson("/api/orders/{$order->getKey()}/cancel")
         ->assertNoContent();
+    $order->refresh();
+    $asset->refresh();
+    expect($order->status)->toBe(OrderStatus::CANCELED);
+    expect($asset->locked_amount)->toBeNull();
+    expect($asset->amount)->toEqual($initialAmount + $order->amount);
+});
+
+test('cancel buy order', function () {
+    actingAs($user = user());
+    $initialBalance = $user->balance;
+    $order = Order::factory()->for($user)->create(['status' => OrderStatus::OPEN, 'side' => true]);
+    postJson("/api/orders/{$order->getKey()}/cancel")
+        ->assertNoContent();
+    $order->refresh();
+    $user->refresh();
+    expect($order->status)->toBe(OrderStatus::CANCELED);
+    expect($user->balance)->toEqual($initialBalance + ($order->price * $order->amount));
 });
