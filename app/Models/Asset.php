6312c6c230d6782c8cff6ce1636eab1684af27c9
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2025-12-16 06:56:13 +0700

    feat(asset): add symbol casting

--- app/Models/Asset.php
+++ app/Models/Asset.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\Symbol;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,5 +10,12 @@ class Asset extends Model
 {
     /** @use HasFactory<\Database\Factories\AssetFactory> */
     use HasFactory;
+
+    protected function casts()
+    {
+        return [
+            'symbol' => Symbol::class,
+        ];
+    }
     public function user(){return $this->belongsTo(User::class);}
 }
