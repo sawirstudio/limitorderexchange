efe250f734a0c35ec10c875973ecbac3b87b1caa
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2025-12-17 00:24:31 +0700

    add docs api

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,13 @@
 namespace App\Providers;
 
 use Carbon\CarbonImmutable;
+use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
@@ -42,5 +46,14 @@ class AppServiceProvider extends ServiceProvider
             Http::preventStrayRequests();
             Sleep::fake();
         }
+        Gate::define('viewApiDocs', function ($user) {
+            return true;
+        });
+        Scramble::configure()
+            ->withDocumentTransformers(function (OpenApi $openApi) {
+                $openApi->secure(
+                    SecurityScheme::http('bearer')
+                );
+            });
     }
 }
