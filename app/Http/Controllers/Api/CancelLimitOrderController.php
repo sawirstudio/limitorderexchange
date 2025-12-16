4a95d9ef95618ca0cf12c6b463100a8b978de6ac
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2025-12-17 00:20:58 +0700

    chore

--- app/Http/Controllers/Api/CancelLimitOrderController.php
+++ app/Http/Controllers/Api/CancelLimitOrderController.php
@@ -4,15 +4,13 @@ namespace App\Http\Controllers\Api;
 
 use App\Enums\OrderStatus;
 use App\Http\Controllers\Controller;
-use App\Models\Asset;
 use App\Models\Order;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\ValidationException;
 
 class CancelLimitOrderController extends Controller
 {
-    public function __invoke(Order $order, Request $request)
+    public function __invoke(Order $order)
     {
         match ($order->status) {
             OrderStatus::CANCELED => throw ValidationException::withMessages([
@@ -24,7 +22,7 @@ class CancelLimitOrderController extends Controller
             default => null,
         };
 
-        DB::select("with ord as (
+        DB::select('with ord as (
         update orders set status = 3 where id = ? returning user_id, symbol, side, (price * amount) as volume
         ),
         release_locked_asset as (
@@ -33,7 +31,7 @@ class CancelLimitOrderController extends Controller
         release_locked_balance as (
             update users set balance = balance + (select volume from ord) where exists(select 1 from ord where ord.side is true and ord.user_id = users.id) returning balance
         )
-        select 1", [$order->getKey()]);
+        select 1', [$order->getKey()]);
 
         return response()->noContent();
     }
